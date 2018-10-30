7a804316c64f9c07b1cd9fac062a492d8288caaf
Ketil Velle <ketil.s.velle@nav.no>
2018-10-30 10:10:32 +0100

    WiP for å få deploy til nais til å fungere

---
+++ src/test/java/no/nav/sbl/dialogarena/modiasoaprest/MainTest.java
@@ -0,0 +1,18 @@
+package no.nav.sbl.dialogarena.modiasoaprest;
+
+import no.nav.sbl.dialogarena.modiasoaprest.config.AppContextTest;
+import no.nav.testconfig.ApiAppTest;
+
+class MainTest {
+
+    public static final String TEST_PORT = "8802";
+
+    public static void main(String[] args) {
+        AppContextTest.setUp();
+
+        ApiAppTest.setupTestContext(ApiAppTest.Config.builder().applicationName("modia-soap-til-rest-proxy").build());
+
+        Main.main(TEST_PORT);
+    }
+
+}
--- src/test/java/no/nav/sbl/dialogarena/modiasoaprest/config/AppContextTest.java
+++ src/test/java/no/nav/sbl/dialogarena/modiasoaprest/config/AppContextTest.java
@@ -1,5 +1,13 @@
 package no.nav.sbl.dialogarena.modiasoaprest.config;
 
+import no.nav.brukerdialog.security.Constants;
+import no.nav.brukerdialog.tools.SecurityConstants;
+import no.nav.dialogarena.config.fasit.FasitUtils;
+import no.nav.dialogarena.config.fasit.ServiceUser;
+import no.nav.dialogarena.config.fasit.dto.RestService;
+import no.nav.dialogarena.config.util.Util;
+import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
+import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,16 +15,52 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static java.lang.System.setProperty;
+import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.AZUREAD_B2C_DISCOVERY_URL_PROPERTY_NAME;
+import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.AZUREAD_B2C_EXPECTED_AUDIENCE_PROPERTY_NAME;
+import static no.nav.dialogarena.config.fasit.FasitUtils.Zone.FSS;
+import static no.nav.dialogarena.config.fasit.FasitUtils.getDefaultEnvironment;
+import static no.nav.sbl.dialogarena.common.abac.pep.service.AbacServiceConfig.ABAC_ENDPOINT_URL_PROPERTY_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ApplicationContext.class})
 public class AppContextTest {
 
+    public static final String APPLICATION_NAME = "modia-soap-til-rest-proxy";
+
     @BeforeClass
     public static void setUp() {
-        setProperty("henvendelsesarkiv.informasjon.endpoint.url", "https://modapp-t6.adeo.no/henvendelsesarkiv/services/domene.Brukerdialog/ArkivertHenvendelse_v1");
-        setProperty("henvendelsesarkiv.aktivitet.endpoint.url", "https://modapp-t6.adeo.no/henvendelsesarkiv/services/domene.Brukerdialog/ArkiverHenvendelseBehandling_v1");
+        String securityTokenService = FasitUtils.getBaseUrl("securityTokenService", FSS);
+        ServiceUser srvModiaSoapTilRestProxy = FasitUtils.getServiceUser("srvmodia-soap-til-rest-proxy", APPLICATION_NAME);
+
+        setProperty(StsSecurityConstants.STS_URL_KEY, securityTokenService);
+        setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, srvModiaSoapTilRestProxy.getUsername());
+        setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, srvModiaSoapTilRestProxy.getPassword());
+
+        setProperty(ABAC_ENDPOINT_URL_PROPERTY_NAME, FasitUtils.getRestService("abac.pdp.endpoint", getDefaultEnvironment()).getUrl());
+        setProperty(CredentialConstants.SYSTEMUSER_USERNAME, srvModiaSoapTilRestProxy.getUsername());
+        setProperty(CredentialConstants.SYSTEMUSER_PASSWORD, srvModiaSoapTilRestProxy.getPassword());
+
+        String issoHost = FasitUtils.getBaseUrl("isso-host");
+        String issoJWS = FasitUtils.getBaseUrl("isso-jwks");
+        String issoISSUER = FasitUtils.getBaseUrl("isso-issuer");
+        String issoIsAlive = FasitUtils.getBaseUrl("isso.isalive", FasitUtils.Zone.FSS);
+        ServiceUser isso_rp_user = FasitUtils.getServiceUser("isso-rp-user", APPLICATION_NAME);
+        RestService loginUrl = FasitUtils.getRestService("veilarblogin.redirect-url", "t6");
+
+        setProperty(Constants.ISSO_HOST_URL_PROPERTY_NAME, issoHost);
+        setProperty(Constants.ISSO_RP_USER_USERNAME_PROPERTY_NAME, isso_rp_user.getUsername());
+        setProperty(Constants.ISSO_RP_USER_PASSWORD_PROPERTY_NAME, isso_rp_user.getPassword());
+        setProperty(Constants.ISSO_JWKS_URL_PROPERTY_NAME, issoJWS);
+        setProperty(Constants.ISSO_ISSUER_URL_PROPERTY_NAME, issoISSUER);
+        setProperty(Constants.ISSO_ISALIVE_URL_PROPERTY_NAME, issoIsAlive);
+        setProperty(SecurityConstants.SYSTEMUSER_USERNAME, srvModiaSoapTilRestProxy.getUsername());
+        setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, srvModiaSoapTilRestProxy.getPassword());
+        setProperty(Constants.OIDC_REDIRECT_URL_PROPERTY_NAME, loginUrl.getUrl());
+
+        ServiceUser azureADClientId = FasitUtils.getServiceUser("aad_b2c_clientid", APPLICATION_NAME);
+        Util.setProperty(AZUREAD_B2C_DISCOVERY_URL_PROPERTY_NAME, FasitUtils.getBaseUrl("aad_b2c_discovery"));
+        Util.setProperty(AZUREAD_B2C_EXPECTED_AUDIENCE_PROPERTY_NAME, azureADClientId.username);
     }
 
     @Test
