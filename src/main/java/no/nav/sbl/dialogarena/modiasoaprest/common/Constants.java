0cf534c58be1d751e28aede330afee97746efe53
Ketil Velle <ketil.s.velle@nav.no>
2018-10-30 11:59:03 +0100

    Fjernet startJetty-klasse siden vi har MainTest-klasse som er startpunkt for lokal kjøring

--- src/main/java/no/nav/sbl/dialogarena/modiasoaprest/common/Constants.java
+++ src/main/java/no/nav/sbl/dialogarena/modiasoaprest/common/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     public static final String GRANT_TYPE_PARAM = "urn:ietf:params:oauth:grant-type:token-exchange";
     public static final String REQUESTED_TOKEN_TYPE_PARAM = "urn:ietf:params:oauth:token-type:access_token";
     public static final String SUBJECT_TOKEN_TYPE_PARAM = "urn:ietf:params:oauth:token-type:saml2";
-    public static final String MODIASOAPRESTPROXY_SYSTEM_USER = "srvmodiasoaprestpr";
+    public static final String MODIASOAPRESTPROXY_SYSTEM_USER = "srvmodia-soap-til-";
     public static final String MODIASOAPRESTPROXY_SYSTEM_USER_PASSWORD = System.getProperty("no.nav.modig.security.systemuser.password");
 }
