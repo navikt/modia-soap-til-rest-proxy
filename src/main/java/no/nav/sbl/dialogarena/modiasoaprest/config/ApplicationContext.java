1552153587c6b0f52cd0c3ae4af616219ac0e13b
Ketil Velle <ketil.s.velle@nav.no>
2018-10-30 13:53:16 +0100

    Overstyrer isso-config da username og pw ble feil grunnet langt app-navn

--- src/main/java/no/nav/sbl/dialogarena/modiasoaprest/config/ApplicationContext.java
+++ src/main/java/no/nav/sbl/dialogarena/modiasoaprest/config/ApplicationContext.java
@@ -2,9 +2,14 @@ package no.nav.sbl.dialogarena.modiasoaprest.config;
 
 import no.nav.apiapp.ApiApplication;
 import no.nav.apiapp.config.ApiAppConfigurator;
+import no.nav.apiapp.config.IssoConfig;
+import no.nav.sbl.util.EnvironmentUtils;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import static no.nav.sbl.dialogarena.modiasoaprest.common.Constants.MODIASOAPRESTPROXY_SYSTEM_USER;
+import static no.nav.sbl.dialogarena.modiasoaprest.common.Constants.MODIASOAPRESTPROXY_SYSTEM_USER_PASSWORD;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -14,7 +19,11 @@ public class ApplicationContext implements ApiApplication.NaisApiApplication {
     @Override
     public void configure(ApiAppConfigurator apiAppConfigurator) {
         apiAppConfigurator
-                .issoLogin()
+                .issoLogin(getIssoLoginConfig())
                 .sts();
+    }
+
+    private IssoConfig getIssoLoginConfig() {
+        return IssoConfig.builder().username(MODIASOAPRESTPROXY_SYSTEM_USER).password(MODIASOAPRESTPROXY_SYSTEM_USER_PASSWORD).build();
     }
 }
