79f1b38b7fa616501f6d3d457fdaac2130cc98bc
Nicklas Utgaard <nutgaard@gmail.com>
2020-08-10 15:41:35 +0200

    [OPP-1209] bruke CXFServletWithAuth fra common

--- src/main/java/no/nav/sbl/dialogarena/modiasoaprest/soap/infra/SoapServlet.java
+++ src/main/java/no/nav/sbl/dialogarena/modiasoaprest/soap/infra/SoapServlet.java
@@ -1,36 +1,19 @@
 package no.nav.sbl.dialogarena.modiasoaprest.soap.infra;
 
-import no.nav.common.auth.subject.Subject;
 import no.nav.common.cxf.CXFEndpoint;
-import no.nav.common.cxf.saml.SAMLInInterceptor;
+import no.nav.common.cxf.saml.CXFServletWithAuth;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.logging.FaultListener;
-import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import java.io.IOException;
 import java.util.Map;
 
-import static no.nav.common.auth.subject.SubjectHandler.withSubjectProvider;
-
-public class SoapServlet extends CXFNonSpringServlet {
+public class SoapServlet extends CXFServletWithAuth {
     private final Map<String, Object> endpoints;
 
     public SoapServlet(Map<String, Object> endpoints) {
         this.endpoints = endpoints;
     }
-
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        withSubjectProvider(
-                () -> (Subject) req.getAttribute(SAMLInInterceptor.SUBJECT_REQUEST_ATTRIBUTE_NAME),
-                () -> super.service(req, res)
-        );
-    }
-
 
     @Override
     protected void loadBus(ServletConfig sc) {
